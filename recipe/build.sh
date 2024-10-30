3cee989aaf75f8603d2385f3f57eb0e8f4a7ef0f
jakirkham <jakirkham@gmail.com>
2024-10-30 00:25:56 -0700

    Revamp build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+export CMAKE_CONFIG="Release"
 
-mkdir build
-cd build
-cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -GNinja ..
-ninja install
+mkdir "build_${CMAKE_CONFIG}"
+pushd "build_${CMAKE_CONFIG}"
+
+cmake ${CMAKE_ARGS} \
+      -G Ninja \
+      -D CMAKE_BUILD_TYPE:STRING="${CMAKE_CONFIG}" \
+      -D CMAKE_PREFIX_PATH:PATH="${PREFIX}" \
+      -D CMAKE_INSTALL_PREFIX:PATH="${PREFIX}" \
+      "${SRC_DIR}" \
+;
+cmake --build . --parallel ${CPU_COUNT}
+cmake --install .
+
+popd
